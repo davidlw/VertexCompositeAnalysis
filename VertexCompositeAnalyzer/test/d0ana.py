48183847cc6c8afcb365aa366e600c75d59208d8
Wei <liwei810812@gmail.com>
2018-06-06 22:22:15 +0200

    Update

---
+++ VertexCompositeAnalyzer/test/d0ana.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger.cerr.threshold = 'INFO'
+#process.MessageLogger.categories.append('Demo')
+#process.MessageLogger.cerr.INFO = cms.untracked.PSet(
+#        limit = cms.untracked.int32(-1)
+#        )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+process.options   = cms.untracked.PSet( wantSummary = 
+cms.untracked.bool(True) )
+
+#process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+#process.load("Configuration.Geometry.GeometryIdeal_cff")
+#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = "80X_dataRun2_Prompt_v15"
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) 
+)
+
+process.source = cms.Source("PoolSource",
+                                fileNames = cms.untracked.vstring(
+'root://cms-xrd-global.cern.ch//store/user/davidlw/PAHighMultiplicity1/pPb_Skim_D0Both_v1_test/180523_221148/0000/pPb_HM_81.root',
+                ),
+secondaryFileNames = cms.untracked.vstring(
+'root://cms-xrd-global.cern.ch//store/hidata/PARun2016C/PAHighMultiplicity1/AOD/PromptReco-v1/000/285/480/00000/0C73EB2B-22AF-E611-85F1-02163E013657.root'
+)
+                            )
+
+process.load("VertexCompositeAnalysis.VertexCompositeAnalyzer.d0analyzer_cff")
+
+process.TFileService = cms.Service("TFileService",
+                                       fileName = 
+cms.string('d0ana.root')
+                                   )
+
+
+process.p = cms.Path(process.d0ana)
