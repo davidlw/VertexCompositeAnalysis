0dc383ef9c7d8d7694657ce9e6c04c49ca906492
Wei <liwei810812@gmail.com>
2018-06-20 21:58:10 +0200

    Clearn up

--- VertexCompositeAnalyzer/python/d0analyzer_cff.py
+++
@@ -1,3 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from VertexCompositeAnalysis.VertexCompositeAnalyzer.d0analyzer_cfi import *
--- VertexCompositeAnalyzer/python/dimuanalyzer_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from VertexCompositeAnalysis.VertexCompositeAnalyzer.dimuanalyzer_cfi import *
-
-jpsiana = dimuana.clone()
-jpsiana_mc = dimuana_mc.clone()
-
-
