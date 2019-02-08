65a66e4dcdf1ef7f345e091717c2230ac47de630
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-02-08 23:57:16 +0100

    keep labels about centrality

--- VertexCompositeProducer/python/mtdanalysisSkimContentD0_cff.py
+++ VertexCompositeProducer/python/mtdanalysisSkimContentD0_cff.py
@@ -25,10 +25,10 @@ analysisSkimContent = cms.PSet(
 #      'keep *_conversions*_*_*',
       # muon
 #      'keep recoMuons_muons_*_*',      
-#      'keep *_hiCentrality_*_*',
+      'keep *_hiCentrality_*_*',
 #      'keep *_hiEvtPlane_*_*',
 #      'keep *_hiEvtPlaneFlat_*_*',
-#      'keep *_centralityBin_*_*',
+      'keep *_centralityBin_*_*',
       # V0
       'keep *_generalD0Candidates*_D0_*',
       'keep *_generalD0Candidates*_MVAValues*_*',
