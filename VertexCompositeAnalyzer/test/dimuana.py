145561cdc921d14969f381b3a8a98e8621cb12e7
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2019-04-17 01:57:11 +0200

    Updated the setup for PbPb 2015

--- VertexCompositeAnalyzer/test/dimuana.py
+++ VertexCompositeAnalyzer/test/dimuana.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-from Configuration.StandardSequences.Eras import eras
-process = cms.Process("ANATREE",eras.Run2_2018_pp_on_AA)
+process = cms.Process("ANATREE")
 
 # Limit the output messages
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -10,14 +9,14 @@ cms.untracked.bool(True) )
 
 # Define the input source
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
-#'root://cms-xrd-global.cern.ch//store/user/anstahll/RiceHIN/PbPb2018/SKIM/VertexCompositeSkim_HIDoubleMuonPsiPeri_v2_HIRun2018_DiMuMassMin7_20190407/HIDoubleMuonPsiPeri/VertexCompositeSkim_HIDoubleMuonPsiPeri_v2_HIRun2018_DiMuMassMin7_20190407/190407_181439/0001/PbPb_DiMuCont_1249.root'
-'file:/afs/cern.ch/user/a/anstahll/work/Analysis/GITHUB/CMSSW_10_3_3_patch1/src/VertexCompositeAnalysis/VertexCompositeProducer/test/PbPb_DiMuCont.root')
+'file:/afs/cern.ch/user/a/anstahll/work/Analysis/Production/CMSSW_7_5_9/src/VertexCompositeAnalysis/VertexCompositeProducer/test/PbPb_DiMuCont.root')
 )
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 # Set the global tag
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.GlobalTag.globaltag = cms.string('103X_dataRun2_v6')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '75X_dataRun2_v13', '')
 
 # Add the VertexComposite tree
 process.load("VertexCompositeAnalysis.VertexCompositeAnalyzer.dimuanalyzer_tree_cff")
