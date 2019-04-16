670ca1fc5271d0abff3fb7a41f56a2c46ded2cf9
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2019-04-16 18:15:56 +0200

    Updated the setup for low PU data

--- VertexCompositeProducer/python/generalDiMuCandidates_cff.py
+++ VertexCompositeProducer/python/generalDiMuCandidates_cff.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 from VertexCompositeAnalysis.VertexCompositeProducer.generalDiMuCandidates_cfi import *
 
-HybridSoftIdReco2018 = "(isGlobalMuon && innerTrack.hitPattern.trackerLayersWithMeasurement > 5 && innerTrack.hitPattern.pixelLayersWithMeasurement > 0)"
+SoftIdReco = "(innerTrack.isNonnull && innerTrack.hitPattern.trackerLayersWithMeasurement > 5 && innerTrack.hitPattern.pixelLayersWithMeasurement > 0 && innerTrack.quality(\"highPurity\"))"
 TightIdReco = "(isGlobalMuon && isPFMuon && globalTrack.normalizedChi2 < 10 && globalTrack.hitPattern.numberOfValidMuonHits > 0 && numberOfMatchedStations > 1 && track.hitPattern.trackerLayersWithMeasurement > 5 && track.hitPattern.numberOfValidPixelHits > 0)"
 
-generalMuMuMassMin7Candidates = generalDiMuCandidates.clone(
-    mllCutMin = cms.double(7.0),
-    muonSelection = cms.string("(p > 3.5 && abs(eta) < 2.4) && ("+HybridSoftIdReco2018+" || "+TightIdReco+")"),
-    candidateSelection = cms.string("mass > 7.0")
+generalMuMuMassMin2Candidates = generalDiMuCandidates.clone(
+    mllCutMin = cms.double(2.0),
+    muonSelection = cms.string("(p > 3.5 && abs(eta) < 2.4) && ("+SoftIdReco+" || "+TightIdReco+")"),
+    candidateSelection = cms.string("mass > 2.0")
 )
